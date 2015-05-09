9a33d7c281db5118f2843bb88b646df9fc56a78e
Giovanni Lodi <giovanni.lodi42@gmail.com>
2015-05-09 17:36:18 +1000

    Add umbrella header for ImplicitSignednessKit

---
+++ ImplicitSignednessKit/ImplicitSignednessKit.h
@@ -0,0 +1,7 @@
+#ifndef ImplicitSignednessKit_ImplicitSignednessKit_h
+#define ImplicitSignednessKit_ImplicitSignednessKit_h
+
+#import "NSArray+Signedness.h"
+#import "NSIndexPath+Signedness.h"
+
+#endif
